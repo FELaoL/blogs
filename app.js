599b02f43d0dd38842bb6af3128917cf52cc7bb6
FELaoL <felaol@sina.com>
2022-10-22 11:17:31 +0800

    登录逻辑

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 const express = require("express");
 // 处理路径
 const path = require("path");
+// 引入body-parser模块，用来处理post请求参数
+const bodyParse = require("body-parser");
 // 创建网站服务器
 const app = express();
 // 数据库连接
 require("./model/connect");
+// 处理post请求参数
+app.use(bodyParse.urlencoded({ extended: false }));
 // require("./model/user");
 // 告诉express框架模板所在的位置
 app.set("views", path.join(__dirname, "views"));
@@ -24,4 +28,4 @@ app.use("/home", home);
 app.use("/admin", admin);
 // 监听端口
 app.listen("80");
-console.log("网站服务器启动成功，请访问localhost");
+console.log("网站服务器启动成功，请访问http://localhost/admin/login");
--- route/admin.js
+++ route/admin.js
@@ -1,15 +1,42 @@
 // 引用express框架
 const express = require("express");
+// 导入用户集合构造函数
+const { User } = require("./../model/user");
 // 创建博客展示页面路由
-const home = express.Router();
+const admin = express.Router();
 
-home.get("/login", (req, res) => {
+admin.get("/login", (req, res) => {
 	res.render("admin/login");
 });
+
+admin.post("/login", async (req, res) => {
+	// 接受请求参数
+	const { email, password } = req.body;
+	// 如果用户没有输入邮件地址
+	if (email.trim().length === 0 || password.trim().length === 0) return res.status(400).render("./admin/error", { msg: "邮箱地址或者密码错误" });
+	// 根据邮箱地址查询用户信息
+	// 如果查询到了用户，user变量的值是对象类型，对象中存储的用户信息
+	// 如果没有查询到用户，user变量为空
+	const user = await User.findOne({ email });
+	// 查询到了用户
+	if (user) {
+		//将客户端传递过来的密码和用户信息中的密码进行比对
+		if (password === user.password) {
+			// 登录成功
+			res.send("登录成功");
+		} else {
+			// 没有查询到用户
+			res.status(400).render("./admin/error", { msg: "邮箱地址或者密码错误" });
+		}
+	} else {
+		// 没有查询到用户信息
+		res.status(400).render("./admin/error", { msg: "邮箱地址或者密码错误" });
+	}
+});
 // 创建用户列表路由
-home.get("/user", (req, res) => {
+admin.get("/user", (req, res) => {
 	res.render("admin/user");
 });
 
 // 将路由对象作为模块成员进行导出
-module.exports = home;
+module.exports = admin;
